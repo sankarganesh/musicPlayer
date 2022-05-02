393f98d1cf61d3936ff084016dcc54057839976e
syedbilal2112 <syed.bilal.bs@gmail.com>
2022-05-02 20:59:43 +0530

    Added removeAll Condition in catalog

--- san_music_player/lib/pojo/catalog.dart
+++ san_music_player/lib/pojo/catalog.dart
@@ -58,7 +58,9 @@ class SongDetailModel extends ChangeNotifier {
   }
 
   void removeAll() {
-    _items.clear();
+    if (_items.isNotEmpty) {
+      _items.clear();
+    }
 
     notifyListeners();
   }
